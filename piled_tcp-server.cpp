44c96766870a9406d2e572281f9b3b8255f88d2d
Dennis Marx <dennis_marx@web.de>
2018-04-04 12:07:21 +0200

    Fixed Bug in Server. Enhanced logging
    In previous versions, a client could send unlimited wrong-commands, the connection would stay alive forever.
    This leads to a problem reaching maximum clients, if a misconfigured clients periodically establishes connections to the server, sending wrong commands.
    
    Now a client-connection will be closed by server if clients send 5 wrong commands in an row.
    Furthermore, wrong commands will now be logged to logfile, so that clients can be debugged.

--- piled_tcp-server.cpp
+++ piled_tcp-server.cpp
@@ -1,16 +1,13 @@
 /*
 Kleiner TCP-Server 
-Diese Software, urspr√ºng 2015 geschrieben, stellt einen TCP-Server dar, welcher zur Ausf√ºhrung auf embedded-systemen wie dem Raspberry-Pi gedacht ist.
--Die vorliegende Version wurde angepasst um LED's √ºber die GPIO-Pins des Raspberry-Pi zu steuern. (D. Marx, 28.02.2018)
-
-Dieser Server nutzt die c++_Threadbibliothek um Multithreading (pro client ein Thread) zu erm√∂glichen und die Bibliothek WiringPi (http://wiringpi.com/) zur Ansteuerung der GPIO-Pins des RaspberryPi.
-
+Diese Software, urspr¸ng 2015 geschrieben, stellt einen TCP-Server dar, welcher zur Ausf¸hrung auf embedded-systemen wie dem Raspberry-Pi gedacht ist.
+-Die vorliegende Version wurde angepasst um LED's ¸ber die GPIO-Pins des Raspberry-Pi zu steuern. (D. Marx, 28.02.2018)
+Dieser Server nutzt die c++_Threadbibliothek um Multithreading (pro client ein Thread) zu ermˆglichen und die Bibliothek WiringPi (http://wiringpi.com/) zur Ansteuerung der GPIO-Pins des RaspberryPi.
 Compile with:  
 	old g++:
 		g++ -std=c++0x ./piled_tcp-server.cpp -lpthread -lwiringPi -o dmarxtcp
 	newer g++ 5.3.0:
 		g++ -std=c++17 ./piled_tcp-server.cpp -lpthread -lwiringPi -o dmarxtcp
-
 Autor: D. Marx
 https://github.com/derco0n/PiLED-Server
 */
@@ -44,7 +41,7 @@ int socket_desc, new_socket, c, *new_sock;
 int curclicnt=0; //Arrayindex der aktuellen Clientverbindungen
 std::string connections[maxclients]; //Array of connectionnames
 
-const double myversion=0.27; //Diese Programmversion
+const double myversion=0.281; //Diese Programmversion
 int listenPort = 0; //Auf welchem Port soll gelauscht werden?
 void *connection_handler(void *);
 int connection_id;
@@ -114,7 +111,7 @@ std::string timeStr(void) //Erzeugt einen String mit dem aktuellen Zeitpunkt
 	current = localtime(&now);
 	std::string day, mon, year, hour, min, sec;
 	if (current->tm_mday < 10)
-	{//Falls n√∂tig f√ºhrende Null hinzuf√ºgen
+	{//Falls nˆtig f¸hrende Null hinzuf¸gen
 		day = "0" + itos(current->tm_mday);
 	}
 	else
@@ -122,7 +119,7 @@ std::string timeStr(void) //Erzeugt einen String mit dem aktuellen Zeitpunkt
 		day = itos(current->tm_mday);
 	}
 	if (current->tm_mon+1 < 10)
-	{//Falls n√∂tig f√ºhrende Null hinzuf√ºgen
+	{//Falls nˆtig f¸hrende Null hinzuf¸gen
 		mon = "0" + itos(current->tm_mon+1);
 	}
 	else
@@ -131,7 +128,7 @@ std::string timeStr(void) //Erzeugt einen String mit dem aktuellen Zeitpunkt
 	}
 	year = itos(1900 + current->tm_year); //Jahr
 	if (current->tm_hour < 10)
-	{//Falls n√∂tig f√ºhrende Null hinzuf√ºgen
+	{//Falls nˆtig f¸hrende Null hinzuf¸gen
 		hour = "0" + itos(current->tm_hour);
 	}
 	else
@@ -139,7 +136,7 @@ std::string timeStr(void) //Erzeugt einen String mit dem aktuellen Zeitpunkt
 		hour = itos(current->tm_hour);
 	}	
 	if (current->tm_min < 10)
-	{//Falls n√∂tig f√ºhrende Null hinzuf√ºgen
+	{//Falls nˆtig f¸hrende Null hinzuf¸gen
 		min = "0" + itos(current->tm_min);
 	}
 	else
@@ -147,7 +144,7 @@ std::string timeStr(void) //Erzeugt einen String mit dem aktuellen Zeitpunkt
 		min = itos(current->tm_min);
 	}
 	if (current->tm_sec < 10)
-	{//Falls n√∂tig f√ºhrende Null hinzuf√ºgen
+	{//Falls nˆtig f¸hrende Null hinzuf¸gen
 		sec = "0" + itos(current->tm_sec);
 	}
 	else
@@ -203,23 +200,17 @@ void signal_handler (int sig)
 	/*
 	https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
 	Macro: int SIGTERM
-
     The SIGTERM signal is a generic signal used to cause program termination. 
 	Unlike SIGKILL, this signal can be blocked, handled, and ignored. 
 	It is the normal way to politely ask a program to terminate.
     The shell command kill generates SIGTERM by default. 
-
 Macro: int SIGINT
-
-    The SIGINT (‚Äúprogram interrupt‚Äù) signal is sent when the user types the INTR character (normally C-c).
+    The SIGINT (ìprogram interruptî) signal is sent when the user types the INTR character (normally C-c).
 	See Special Characters, for information about terminal driver support for C-c. 
-
 Macro: int SIGQUIT
-
-    The SIGQUIT signal is similar to SIGINT, except that it‚Äôs controlled by a different key‚Äîthe QUIT character, 
-	usually C-\‚Äîand produces a core dump when it terminates the process, just like a program error signal. 
-	You can think of this as a program error condition ‚Äúdetected‚Äù by the user.
-
+    The SIGQUIT signal is similar to SIGINT, except that itís controlled by a different keyóthe QUIT character, 
+	usually C-\óand produces a core dump when it terminates the process, just like a program error signal. 
+	You can think of this as a program error condition ìdetectedî by the user.
     See Program Error Signals, for information about core dumps. See Special Characters, for information about terminal driver support.
     Certain kinds of cleanups are best omitted in handling SIGQUIT. For example, if the program creates temporary files, it should handle the other termination requests by deleting the temporary files. But it is better for SIGQUIT not to delete them, so that the user can examine them in conjunction with the core dump. 
 	*/
@@ -274,14 +265,14 @@ int main(int argc, char *argv[])
 		}
 		
 		if (pinred<=0 || pingreen<=0 || pinblue<=0 || pinred>=30 || pingreen>=30 || pinblue>=30){
-			//Ung√ºltige Werte. Wert muss zwischen 1 und 29 liegen
+			//Ung¸ltige Werte. Wert muss zwischen 1 und 29 liegen
 			fprintf(stderr,"Invalid pinvalues given! Aborting\n");
 			printHelp();
 			return -3;
 		}
 		
 		if (listenPort <=1024 || listenPort >65535){
-			//Ung√ºltiger Port. Alles bis 1024 ist reseviert. Maximalwert 65535
+			//Ung¸ltiger Port. Alles bis 1024 ist reseviert. Maximalwert 65535
 			fprintf(stderr,"Invalid listen port! Aborting\n");
 			printHelp();
 			return -3;
@@ -313,7 +304,8 @@ int main(int argc, char *argv[])
 		delay (200) ;
 		digitalWrite (pinblue, false);
 
-		writelog("Program started.\n"); //Logeintrag machen
+		std::string startmsg="Program started. Version is "+std::to_string(myversion)+"\n";
+		writelog(startmsg.c_str()); //Logeintrag machen
 		
 		std::stringstream ss;
 		ss << listenPort;		
@@ -358,7 +350,7 @@ int main(int argc, char *argv[])
 		c = sizeof(struct sockaddr_in);
 
 		while (shuttingdown==false && (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)))
-		{//Server f√§hrt gerade nicht herunter, die maximale Clientanzahl ist noch nicht erreicht und die eingehende Verbindung wurde akzeptiert
+		{//Server f‰hrt gerade nicht herunter, die maximale Clientanzahl ist noch nicht erreicht und die eingehende Verbindung wurde akzeptiert
 			
 			pthread_t sniffer_thread; //neuen Thread deklarieren
 			new_sock = (int*)malloc(1);
@@ -449,6 +441,8 @@ int main(int argc, char *argv[])
 //int doStuff(char code[1024], int connid)
 int doStuff(char code[1024], std::string clientconn)
 {
+	std::string msg="";
+	
 	int status = -1;
 	if (
 	(code[0]=='1' || code[0]=='0') &&
@@ -461,18 +455,15 @@ int doStuff(char code[1024], std::string clientconn)
 		//Zielstatus ermitteln
 		bool redstate=charState(code[0]); //Erstes Zeichen = Rotstatus
 		bool greenstate=charState(code[1]); //Zweites Zeichen = Gruenstatus
-		bool bluestate=charState(code[2]); //Drittes Zeichen = Blaustatus
+		bool bluestate=charState(code[2]); //Drittes Zeichen = Blaustatus		
 		
-		std::string msg="";
 		//msg += "Received new LED settings from connection "+std::to_string(connid)+". Code: \"" + std::string(code).substr(0,3) + std::string("\"\n");
-		msg += "Received new LED settings from "+clientconn+" -> Code: \"" + std::string(code).substr(0,3) + std::string("\"\n");
-		
+		msg += "Received new LED settings from "+clientconn+" -> Code: \"" + std::string(code).substr(0,3) + std::string("\"\n");		
 		fprintf(stdout,msg.c_str());
 		writelog(msg.c_str());
 		
-		delay(30); //30ms warten um die das Schaltverm√∂gen der Relais nicht auszureizen
-		//LEDs setzen		
-						
+		delay(30); //30ms warten um das Schaltvermˆgen der Relais nicht auszureizen
+		//LEDs setzen	
 		digitalWrite (pinred, redstate);
 		digitalWrite (pingreen, greenstate);
 		digitalWrite (pinblue, bluestate);	
@@ -491,12 +482,17 @@ int doStuff(char code[1024], std::string clientconn)
 	{
 		status=1; //exitcode
 	}
+	else {
+		msg += "Received unknown command from "+clientconn+" -> Command: \"" + std::string(code) + std::string("\"\n");		
+		fprintf(stdout,msg.c_str());
+		writelog(msg.c_str());
+	}
 	return status;
 }
 
 
 void lightUpPin(int pinnumber, int period){
-	//Setzt den Status eines pins fuer einen gewissen Zeitraum auf 1 und anschlie√üend wieder auf null
+	//Setzt den Status eines pins fuer einen gewissen Zeitraum auf 1 und anschlieﬂend wieder auf null
 	digitalWrite (pinnumber, true);
 	delay(period);
 	digitalWrite (pinnumber, false);
@@ -538,9 +534,12 @@ void *connection_handler(void *socket_desc)
 	char client_message[1024];
 	char reciev[1024];
     int sendStatus=0; 
-	char result[100];		
+	char result[100];	
+
+	int cmderror=0; //counts wrong commands
+	const int maxcmderror=5; //how much wrong commands could be received before closing the connection
 	
-	//connections[connid]=sock; //Dieses Socket dem Socketarray hinzuf√ºgen
+	//connections[connid]=sock; //Dieses Socket dem Socketarray hinzuf¸gen
 	
     //Send some messages to the client
     message = "LED-Control ready. Please tell me what to do.\n";
@@ -552,7 +551,7 @@ void *connection_handler(void *socket_desc)
 	std::string clientconn=cip+":"+std::to_string(cport);
 	*/
  
-	while(shuttingdown==false && (read_size = recv(sock , client_message , 1024 , 0)) > 0 ) //Solange gr√∂√üer 0, besteht die Verbindung zum Client
+	while(shuttingdown==false && (read_size = recv(sock , client_message , 1024 , 0)) > 0 ) //Solange grˆﬂer 0, besteht die Verbindung zum Client
     {
 		/*
 		//DEBUG
@@ -567,20 +566,33 @@ void *connection_handler(void *socket_desc)
 		int retval=doStuff(client_message, clientconn);
 		if (retval==0){ 
 		message = "command ok\n";
+		cmderror=0; //Reset Commanderror-counter
 		}
-		else if (retval==1 || shuttingdown==true){
-			//Verbindung soll beendet werden
+		else if (retval==1 /*Client sent "exit"*/|| shuttingdown==true /*Server was aborted via "CTRL-C"*/){
+			//Connection should be closed
 			//Free the socket pointer
 			message = "good bye\n";	
+			write(sock, message.c_str(), strlen(message.c_str())); //Send message to the client
 		}
-		else {
-			message = "command error\n";
+		else {			
+			cmderror++; //increase Commanderror-counter
+			if (cmderror>=maxcmderror){
+				message = "Maximum command error count reached. Closing Connection.\n";
+				write(sock, message.c_str(), strlen(message.c_str())); //Send message to the client
+				writelog(message.c_str());
+				
+				//Closemessage
+				message = "good bye\n";	
+				write(sock, message.c_str(), strlen(message.c_str())); //Send message to the client
+			}
+			else {
+				message = "Command error (Count: "+std::to_string(cmderror)+")\n";
+				write(sock, message.c_str(), strlen(message.c_str())); //Send message to the client
+				writelog(message.c_str());
+			}
 		}
-		
-		//Answer
-		write(sock, message.c_str(), strlen(message.c_str())); //Send message to the client
-		
-		if (retval==1 || shuttingdown==true){
+				
+		if (cmderror>=maxcmderror /*Maximum command errors reached*/|| retval==1 /*Client sent "exit"*/|| shuttingdown==true /*Server was aborted via "CTRL-C"*/){
 			//Close connection 
 			shutdown(sock, SHUT_RDWR);	
 			std::string logmsg ="connection ("+clientconn+") closed by server.\n";				
@@ -591,18 +603,18 @@ void *connection_handler(void *socket_desc)
 		}
     }
      
-    if(read_size == 0)//Client hat die Verbindung getrennt
+    if(read_size == 0)//Client closed connection
     {
 		std::string logmsg ="connection ("+clientconn+") closed by client.\n";				
 		fprintf(stdout,logmsg.c_str());	
-        writelog(logmsg.c_str()); //Logeintrag machen		
+        writelog(logmsg.c_str()); //make log		
         //fflush(stdout);
     }
-    else if(read_size == -1) //Verbindung wurde unterbrochen.
+    else if(read_size == -1) //connection was aborted by network
     {
         std::string logmsg ="connection ("+clientconn+") aborted.\n";				
 		fprintf(stdout,logmsg.c_str());	
-        writelog(logmsg.c_str()); //Logeintrag machen		
+        writelog(logmsg.c_str()); //make log
     }
          
     //Free the socket pointer
@@ -611,8 +623,11 @@ void *connection_handler(void *socket_desc)
 	//closeSocket(sock);
     free(socket_desc);
 	
-	curclicnt-=1; //Clientverbindungen um eine reduzieren
+	curclicnt-=1; //reduce active clientconnectioncounter by 1
 	
-	fprintf(stdout,"%d active clients.\n",curclicnt);
+	std::string clientsmsg=std::to_string(curclicnt)+" active clients.\n";
+	//fprintf(stdout,"%d active clients.\n",curclicnt);
+	fprintf(stdout, clientsmsg.c_str());
+	writelog(clientsmsg.c_str()); //make log
     return 0;
 }
